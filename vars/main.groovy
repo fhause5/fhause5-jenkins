787e45e31022a5c1037d18863305beaba3c6dd97
Igor <45092677+fhause5@users.noreply.github.com>
2020-08-17 16:52:50 +0300

    Update main.groovy

--- vars/main.groovy
+++ vars/main.groovy
@@ -1,7 +1,11 @@
 import org.devops.*;
 
-def call() {
-    println('Heee')
-   // Configuration.test()
-
+def call(text) {
+    try {
+        sh "echo ${text} "
+    } catch(Exception e) {
+        echo "does not work"
+        echo "The build is failed"
+        currentBuild.result = 'FAILURE'
+    }
 }
